ef4d45195ae1c73d4ecdb0d290e0e6a2a35737c3
hisama <56408231+hisama@users.noreply.github.com>
2026-04-03 21:28:28 +0200

    Update index.tsx

--- src/frame/index.tsx
+++ src/frame/index.tsx
@@ -5,24 +5,19 @@ export function Frame() {
     <header className={`frame ${styles.frame}`}>
       <h1 className={styles.frame__title}>Marco Cascella</h1>
       <a className={styles.frame__back} href="https://tympanus.net/codrops/?p=106679">
-        Article
+        Home
       </a>
       <a className={styles.frame__archive} href="https://tympanus.net/codrops/hub/">
-        All demos
+        Bio
       </a>
       <a className={styles.frame__github} href="https://github.com/edoardolunardi/infinite-canvas">
-        GitHub
+        Contacts
       </a>
       <div className={styles.frame__credits}>
-        <span>By </span>
-        <a href="https://www.edoardolunardi.dev/">Edoardo Lunardi</a>
+        <span></span>
+        <a href="#"></a>
       </div>
       <nav className={styles.frame__tags}>
-        <a href="https://tympanus.net/codrops/demos/?tag=scroll">#scroll</a>
-        <a href="https://tympanus.net/codrops/demos/?tag=infinite">#inifinite</a>
-        <a href="https://tympanus.net/codrops/demos/?tag=draggable">#draggable</a>
-        <a href="https://tympanus.net/codrops/demos/?tag=three-js">#three.js</a>
-        <a href="https://tympanus.net/codrops/demos/?tag=webgl">#webgl</a>
       </nav>
     </header>
   );
